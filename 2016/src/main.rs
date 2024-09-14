d979aa311730cd87ddffafcb45378657d92bdaf4
bbremer <bennettjamesbremer@gmail.com>
2024-09-14 11:08:57 -0400

    Add day 5 to main.rs

--- 2016/src/main.rs
+++ 2016/src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn run_day(n: u8, f: fn(&str) -> (String, String)) {
     let now = std::time::Instant::now();
@@ -15,4 +16,5 @@ fn main() {
     run_day(2, day2::run);
     run_day(3, day3::run);
     run_day(4, day4::run);
+    run_day(4, day5::run);
 }
