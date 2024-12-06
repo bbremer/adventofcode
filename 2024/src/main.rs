79429ddc8a8b6de192ac0507445c790dd72ad343
bbremer <bennettjamesbremer@gmail.com>
2024-12-05 22:47:43 -0500

    almost done with 2024 day2

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -10,48 +10,72 @@ fn main() {
             .lines()
             .map(|line| analyze_line(line.unwrap()))
             .fold((0, 0), |(part1, part2), (part1_safety, part2_safety)| {
-                (
-                    part1 + matches!(part1_safety, LineSafety::Safe) as i32,
-                    part2 + matches!(part2_safety, LineSafety::Safe) as i32,
-                )
+                (part1 + part1_safety, part2 + part2_safety)
             });
     println!("Part 1: {part1}");
     println!("Part 2: {part2}");
 }
 
-fn analyze_line(line: String) -> (LineSafety, LineSafety) {
+fn analyze_line(line: String) -> (i32, i32) {
     let iter: LineSafetyIter<_> = line
         .as_str()
         .split(" ")
         .map(|num_str| num_str.parse::<i32>().unwrap())
         .into();
-    let unsafe_count = iter
-        .filter(|safety| matches!(safety, LineSafety::Unsafe))
-        .take(2)
-        .count();
-    match unsafe_count {
-        0 => (LineSafety::Safe, LineSafety::Safe),
-        1 => (LineSafety::Unsafe, LineSafety::Safe),
-        2 => (LineSafety::Unsafe, LineSafety::Unsafe),
-        _ => {
-            panic!();
-        }
+    match iter.last() {
+        Some(LineSafety::Safe) => (1, 1),
+        Some(LineSafety::Part2Safe) => (0, 1),
+        Some(LineSafety::Unsafe) => (0, 0),
+        None => panic!(),
     }
 }
 
+#[derive(Copy, Clone)]
 enum LineSafety {
     Safe,
+    Part2Safe,
     Unsafe,
 }
 
 enum Variance {
     Initialized,
-    Increasing,
+    SeenOne(i32),
+    SeenTwo(i32, i32),
+    SeenThree(LevelDifference, LevelDifference, i32),
+    Decreasing(i32, LineSafety),
+    Increasing(i32, LineSafety),
+    Unsafe,
+}
+
+#[derive(Copy, Clone, PartialEq)]
+enum LevelDifference {
     Decreasing,
+    Increasing,
+    Invalid,
+}
+
+impl LevelDifference {
+    fn new(curr: i32, next: i32) -> Self {
+        let diff = next - curr;
+        if -3 <= diff && diff <= -1 {
+            LevelDifference::Decreasing
+        } else if 1 <= diff && diff <= 3 {
+            LevelDifference::Increasing
+        } else {
+            LevelDifference::Invalid
+        }
+    }
+
+    fn into_variance(&self, next: i32, safety: LineSafety) -> Variance {
+        match self {
+            LevelDifference::Decreasing => Variance::Decreasing(next, safety),
+            LevelDifference::Increasing => Variance::Increasing(next, safety),
+            _ => panic!(),
+        }
+    }
 }
 
 struct LineSafetyIter<I> {
-    curr: Option<i32>,
     variance: Variance,
     iter: I,
 }
@@ -60,46 +84,102 @@ impl<I: Iterator<Item = i32>> Iterator for LineSafetyIter<I> {
     type Item = LineSafety;
 
     fn next(&mut self) -> Option<LineSafety> {
-        if self.curr.is_none() {
-            self.curr = Some(self.iter.next().unwrap());
-            return Some(LineSafety::Safe);
-        }
-        let curr = self.curr.unwrap();
-
-        self.iter.next().map(|next| {
-            let diff = next - curr;
-            match self.variance {
-                Variance::Initialized => {
-                    self.variance = if -3 <= diff && diff <= -1 {
-                        Variance::Decreasing
-                    } else if 1 <= diff && diff <= 3 {
-                        Variance::Increasing
-                    } else {
-                        return LineSafety::Unsafe;
-                    };
-                }
-                Variance::Increasing => {
-                    if diff < 1 || 3 < diff {
-                        return LineSafety::Unsafe;
+        match self.iter.next() {
+            None => None,
+            Some(next) => {
+                let (variance, ret) = match self.variance {
+                    Variance::Initialized => (Variance::SeenOne(next), LineSafety::Safe),
+                    Variance::SeenOne(curr) => match LevelDifference::new(curr, next) {
+                        LevelDifference::Decreasing => (
+                            Variance::Decreasing(next, LineSafety::Safe),
+                            LineSafety::Safe,
+                        ),
+                        LevelDifference::Increasing => (
+                            Variance::Increasing(next, LineSafety::Safe),
+                            LineSafety::Safe,
+                        ),
+                        LevelDifference::Invalid => {
+                            (Variance::SeenTwo(curr, next), LineSafety::Part2Safe)
+                        }
+                    },
+                    Variance::SeenTwo(l1, l2) => {
+                        let diff1 = LevelDifference::new(l1, next);
+                        let diff2 = LevelDifference::new(l2, next);
+                        match (diff1, diff2) {
+                            (LevelDifference::Invalid, LevelDifference::Invalid) => {
+                                (Variance::Unsafe, LineSafety::Unsafe)
+                            }
+                            (l1_diff, LevelDifference::Invalid) => (
+                                l1_diff.into_variance(next, LineSafety::Part2Safe),
+                                LineSafety::Part2Safe,
+                            ),
+                            (LevelDifference::Invalid, l2_diff) => (
+                                l2_diff.into_variance(next, LineSafety::Part2Safe),
+                                LineSafety::Part2Safe,
+                            ),
+                            _ => (
+                                Variance::SeenThree(diff1, diff2, next),
+                                LineSafety::Part2Safe,
+                            ),
+                        }
                     }
-                }
-                Variance::Decreasing => {
-                    if diff < -3 || -1 < diff {
-                        return LineSafety::Unsafe;
+                    Variance::SeenThree(diff1, diff2, l3) => {
+                        let diff3 = LevelDifference::new(l3, next);
+                        if diff3 == diff1 || diff3 == diff2 {
+                            (
+                                diff3.into_variance(next, LineSafety::Part2Safe),
+                                LineSafety::Part2Safe,
+                            )
+                        } else {
+                            (Variance::Unsafe, LineSafety::Unsafe)
+                        }
                     }
-                }
+                    Variance::Decreasing(curr, safety) => {
+                        let diff = LevelDifference::new(curr, next);
+                        match diff {
+                            LevelDifference::Decreasing => {
+                                (diff.into_variance(next, safety), safety)
+                            }
+                            _ => match safety {
+                                LineSafety::Safe => (
+                                    Variance::Decreasing(curr, LineSafety::Part2Safe),
+                                    LineSafety::Part2Safe,
+                                ),
+                                LineSafety::Part2Safe => (Variance::Unsafe, LineSafety::Unsafe),
+                                _ => panic!(),
+                            },
+                        }
+                    }
+                    Variance::Increasing(curr, safety) => {
+                        let diff = LevelDifference::new(curr, next);
+                        match diff {
+                            LevelDifference::Increasing => {
+                                (diff.into_variance(next, safety), safety)
+                            }
+                            _ => match safety {
+                                LineSafety::Safe => (
+                                    Variance::Increasing(curr, LineSafety::Part2Safe),
+                                    LineSafety::Part2Safe,
+                                ),
+                                LineSafety::Part2Safe => (Variance::Unsafe, LineSafety::Unsafe),
+                                _ => panic!(),
+                            },
+                        }
+                    }
+                    Variance::Unsafe => {
+                        return None;
+                    }
+                };
+                self.variance = variance;
+                Some(ret)
             }
-            self.curr = Some(next);
-
-            LineSafety::Safe
-        })
+        }
     }
 }
 
 impl<I: Iterator<Item = i32>> From<I> for LineSafetyIter<I> {
     fn from(iter: I) -> Self {
         LineSafetyIter {
-            curr: None,
             variance: Variance::Initialized,
             iter,
         }
