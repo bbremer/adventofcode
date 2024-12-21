63f851489907384843fc30b179a1daef4ff4ab12
bbremer <bennettjamesbremer@gmail.com>
2024-12-21 13:18:34 -0500

    2024 day 2

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -6,12 +6,18 @@ fn main() {
     // day1::run();
 
     let (part1, part2) =
+        // std::io::BufReader::new(std::fs::File::open("../inputs/test.txt").unwrap())
         std::io::BufReader::new(std::fs::File::open("../inputs/day2.txt").unwrap())
             .lines()
-            .map(|line| analyze_line(line.unwrap()))
-            .fold((0, 0), |(part1, part2), (part1_safety, part2_safety)| {
-                (part1 + part1_safety, part2 + part2_safety)
-            });
+            .map(|line| {
+                analyze_line(line.unwrap())
+            })
+            .fold(
+                (0, 0),
+                |(part1, part2), (part1_safety, part2_safety)| {
+                    (part1 + part1_safety, part2 + part2_safety)
+                },
+            );
     println!("Part 1: {part1}");
     println!("Part 2: {part2}");
 }
@@ -30,20 +36,21 @@ fn analyze_line(line: String) -> (i32, i32) {
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 enum LineSafety {
     Safe,
     Part2Safe,
     Unsafe,
 }
 
-enum Variance {
+#[derive(Copy, Clone, Debug)]
+enum LineSafetyIterState {
     Initialized,
     SeenOne(i32),
     SeenTwo(i32, i32),
-    SeenThree(LevelDifference, LevelDifference, i32),
-    Decreasing(i32, LineSafety),
-    Increasing(i32, LineSafety),
+    SeenThreeConflicting(i32, i32, i32),
+    Decreasing(i32, i32, LineSafety),
+    Increasing(i32, i32, LineSafety),
     Unsafe,
 }
 
@@ -65,18 +72,10 @@ impl LevelDifference {
             LevelDifference::Invalid
         }
     }
-
-    fn into_variance(&self, next: i32, safety: LineSafety) -> Variance {
-        match self {
-            LevelDifference::Decreasing => Variance::Decreasing(next, safety),
-            LevelDifference::Increasing => Variance::Increasing(next, safety),
-            _ => panic!(),
-        }
-    }
 }
 
 struct LineSafetyIter<I> {
-    variance: Variance,
+    state: LineSafetyIterState,
     iter: I,
 }
 
@@ -84,95 +83,150 @@ impl<I: Iterator<Item = i32>> Iterator for LineSafetyIter<I> {
     type Item = LineSafety;
 
     fn next(&mut self) -> Option<LineSafety> {
-        match self.iter.next() {
-            None => None,
-            Some(next) => {
-                let (variance, ret) = match self.variance {
-                    Variance::Initialized => (Variance::SeenOne(next), LineSafety::Safe),
-                    Variance::SeenOne(curr) => match LevelDifference::new(curr, next) {
-                        LevelDifference::Decreasing => (
-                            Variance::Decreasing(next, LineSafety::Safe),
+        match (self.iter.next(), self.state) {
+            (None, _) | (_, LineSafetyIterState::Unsafe) => None,
+            (Some(next), current_state) => match current_state {
+                LineSafetyIterState::Initialized => {
+                    self.state = LineSafetyIterState::SeenOne(next);
+                    Some(LineSafety::Safe)
+                }
+                LineSafetyIterState::SeenOne(curr) => {
+                    let diff = LevelDifference::new(curr, next);
+                    let safety = match diff {
+                        LevelDifference::Decreasing | LevelDifference::Increasing => {
+                            LineSafety::Safe
+                        }
+                        _ => LineSafety::Part2Safe,
+                    };
+                    self.state = LineSafetyIterState::SeenTwo(curr, next);
+                    Some(safety)
+                }
+                LineSafetyIterState::SeenTwo(prev, curr) => {
+                    let diff1 = LevelDifference::new(prev, curr);
+                    let diff2 = LevelDifference::new(curr, next);
+                    let (state, safety) = match (diff1, diff2) {
+                        (LevelDifference::Decreasing, LevelDifference::Decreasing) => (
+                            LineSafetyIterState::Decreasing(curr, next, LineSafety::Safe),
                             LineSafety::Safe,
                         ),
-                        LevelDifference::Increasing => (
-                            Variance::Increasing(next, LineSafety::Safe),
+                        (LevelDifference::Increasing, LevelDifference::Increasing) => (
+                            LineSafetyIterState::Increasing(curr, next, LineSafety::Safe),
                             LineSafety::Safe,
                         ),
-                        LevelDifference::Invalid => {
-                            (Variance::SeenTwo(curr, next), LineSafety::Part2Safe)
+                        _ => (
+                            LineSafetyIterState::SeenThreeConflicting(prev, curr, next),
+                            LineSafety::Part2Safe,
+                        ),
+                    };
+                    self.state = state;
+                    Some(safety)
+                }
+                LineSafetyIterState::SeenThreeConflicting(p0, p1, p2) => {
+                    match triple_safety(p1, p2, next) {
+                        LevelDifference::Decreasing => {
+                            self.state =
+                                LineSafetyIterState::Decreasing(p2, next, LineSafety::Part2Safe);
+                            return Some(LineSafety::Part2Safe);
                         }
-                    },
-                    Variance::SeenTwo(l1, l2) => {
-                        let diff1 = LevelDifference::new(l1, next);
-                        let diff2 = LevelDifference::new(l2, next);
-                        match (diff1, diff2) {
-                            (LevelDifference::Invalid, LevelDifference::Invalid) => {
-                                (Variance::Unsafe, LineSafety::Unsafe)
-                            }
-                            (l1_diff, LevelDifference::Invalid) => (
-                                l1_diff.into_variance(next, LineSafety::Part2Safe),
-                                LineSafety::Part2Safe,
-                            ),
-                            (LevelDifference::Invalid, l2_diff) => (
-                                l2_diff.into_variance(next, LineSafety::Part2Safe),
-                                LineSafety::Part2Safe,
-                            ),
-                            _ => (
-                                Variance::SeenThree(diff1, diff2, next),
-                                LineSafety::Part2Safe,
-                            ),
+                        LevelDifference::Increasing => {
+                            self.state =
+                                LineSafetyIterState::Increasing(p2, next, LineSafety::Part2Safe);
+                            return Some(LineSafety::Part2Safe);
                         }
+                        LevelDifference::Invalid => {}
                     }
-                    Variance::SeenThree(diff1, diff2, l3) => {
-                        let diff3 = LevelDifference::new(l3, next);
-                        if diff3 == diff1 || diff3 == diff2 {
-                            (
-                                diff3.into_variance(next, LineSafety::Part2Safe),
-                                LineSafety::Part2Safe,
-                            )
-                        } else {
-                            (Variance::Unsafe, LineSafety::Unsafe)
+
+                    match triple_safety(p0, p2, next) {
+                        LevelDifference::Decreasing => {
+                            self.state =
+                                LineSafetyIterState::Decreasing(p2, next, LineSafety::Part2Safe);
+                            return Some(LineSafety::Part2Safe);
                         }
+                        LevelDifference::Increasing => {
+                            self.state =
+                                LineSafetyIterState::Increasing(p2, next, LineSafety::Part2Safe);
+                            return Some(LineSafety::Part2Safe);
+                        }
+                        LevelDifference::Invalid => {}
                     }
-                    Variance::Decreasing(curr, safety) => {
-                        let diff = LevelDifference::new(curr, next);
-                        match diff {
-                            LevelDifference::Decreasing => {
-                                (diff.into_variance(next, safety), safety)
-                            }
-                            _ => match safety {
-                                LineSafety::Safe => (
-                                    Variance::Decreasing(curr, LineSafety::Part2Safe),
+
+                    match triple_safety(p0, p1, next) {
+                        LevelDifference::Decreasing => {
+                            self.state =
+                                LineSafetyIterState::Decreasing(p1, next, LineSafety::Part2Safe);
+                            return Some(LineSafety::Part2Safe);
+                        }
+                        LevelDifference::Increasing => {
+                            self.state =
+                                LineSafetyIterState::Increasing(p1, next, LineSafety::Part2Safe);
+                            return Some(LineSafety::Part2Safe);
+                        }
+                        LevelDifference::Invalid => {}
+                    }
+
+                    self.state = LineSafetyIterState::Unsafe;
+                    return Some(LineSafety::Unsafe);
+                }
+                LineSafetyIterState::Decreasing(prev, curr, current_safety) => {
+                    let diff = LevelDifference::new(curr, next);
+                    match diff {
+                        LevelDifference::Decreasing => {
+                            self.state =
+                                LineSafetyIterState::Decreasing(curr, next, current_safety);
+                            Some(current_safety)
+                        }
+                        _ => match current_safety {
+                            LineSafety::Safe => {
+                                let new_curr = match LevelDifference::new(prev, next) {
+                                    LevelDifference::Decreasing => next,
+                                    _ => curr,
+                                };
+                                self.state = LineSafetyIterState::Decreasing(
+                                    prev,
+                                    new_curr,
                                     LineSafety::Part2Safe,
-                                ),
-                                LineSafety::Part2Safe => (Variance::Unsafe, LineSafety::Unsafe),
-                                _ => panic!(),
-                            },
-                        }
-                    }
-                    Variance::Increasing(curr, safety) => {
-                        let diff = LevelDifference::new(curr, next);
-                        match diff {
-                            LevelDifference::Increasing => {
-                                (diff.into_variance(next, safety), safety)
+                                );
+                                Some(LineSafety::Part2Safe)
                             }
-                            _ => match safety {
-                                LineSafety::Safe => (
-                                    Variance::Increasing(curr, LineSafety::Part2Safe),
-                                    LineSafety::Part2Safe,
-                                ),
-                                LineSafety::Part2Safe => (Variance::Unsafe, LineSafety::Unsafe),
-                                _ => panic!(),
-                            },
+                            LineSafety::Part2Safe => {
+                                self.state = LineSafetyIterState::Unsafe;
+                                Some(LineSafety::Unsafe)
+                            }
+                            _ => panic!(),
+                        },
+                    }
+                }
+                LineSafetyIterState::Increasing(prev, curr, current_safety) => {
+                    let diff = LevelDifference::new(curr, next);
+                    match diff {
+                        LevelDifference::Increasing => {
+                            self.state =
+                                LineSafetyIterState::Increasing(curr, next, current_safety);
+                            Some(current_safety)
                         }
+                        _ => match current_safety {
+                            LineSafety::Safe => {
+                                let new_curr = match LevelDifference::new(prev, next) {
+                                    LevelDifference::Increasing => next,
+                                    _ => curr,
+                                };
+                                self.state = LineSafetyIterState::Increasing(
+                                    prev,
+                                    new_curr,
+                                    LineSafety::Part2Safe,
+                                );
+                                Some(LineSafety::Part2Safe)
+                            }
+                            LineSafety::Part2Safe => {
+                                self.state = LineSafetyIterState::Unsafe;
+                                Some(LineSafety::Unsafe)
+                            }
+                            _ => panic!(),
+                        },
                     }
-                    Variance::Unsafe => {
-                        return None;
-                    }
-                };
-                self.variance = variance;
-                Some(ret)
-            }
+                }
+                LineSafetyIterState::Unsafe => panic!(),
+            },
         }
     }
 }
@@ -180,8 +234,18 @@ impl<I: Iterator<Item = i32>> Iterator for LineSafetyIter<I> {
 impl<I: Iterator<Item = i32>> From<I> for LineSafetyIter<I> {
     fn from(iter: I) -> Self {
         LineSafetyIter {
-            variance: Variance::Initialized,
+            state: LineSafetyIterState::Initialized,
             iter,
         }
+    }
+}
+
+fn triple_safety(x: i32, y: i32, z: i32) -> LevelDifference {
+    let diff1 = LevelDifference::new(x, y);
+    let diff2 = LevelDifference::new(y, z);
+    match (diff1, diff2) {
+        (LevelDifference::Increasing, LevelDifference::Increasing) => LevelDifference::Increasing,
+        (LevelDifference::Decreasing, LevelDifference::Decreasing) => LevelDifference::Decreasing,
+        _ => LevelDifference::Invalid,
     }
 }
